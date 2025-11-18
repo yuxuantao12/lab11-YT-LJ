f09306ae3b611a8aeae1da74a370df91a3187080
Yuxuan Tao <144768397+yuxuantao12@users.noreply.github.com>
2025-11-18 11:34:15 -0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -5,6 +5,36 @@
 import math
 
 
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def mul(a, b):
+    return a * b
+
+
+def div(a, b):
+    if a == 0:
+        raise ZeroDivisionError("Cannot divide by zero (a == 0).")
+    return b / a
+
+
+def logarithm(a, b):
+    if a <= 0 or a == 1:
+        raise ValueError("Log base must be positive and not equal to 1.")
+    if b <= 0:
+        raise ValueError("Logarithm argument must be positive.")
+    return math.log(b, a)
+
+
+def exp(a, b):
+    return a ** b
+
+
 def square_root(a):
     try:
         if a < 0:
@@ -19,36 +49,3 @@ def hypotenuse(a, b):
         return math.hypot(a, b)
     except Exception as e:
         raise e
-
-
-def add(a, b):
-    return a + b
-
-
-def subtract(a, b):
-    return a - b
-
-
-def multiply(a, b):
-    return a * b
-
-
-def divide(a, b):
-    if a == 0:
-        raise ZeroDivisionError("Cannot divide by zero (a == 0).")
-    return b / a
-
-
-def logarithm(a, b):
-    if a <= 0 or a == 1:
-        raise ValueError("Log base must be positive and not equal to 1.")
-    if b <= 0:
-        raise ValueError("Logarithm argument must be positive.")
-    return math.log(b, a)
-
-
-def exponent(a, b):
-    return a ** b
-
-
-
