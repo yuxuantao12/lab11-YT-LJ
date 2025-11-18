10ece4984d6cfb6bb89b6dcefdffd0dc20666c22
Yuxuan Tao <144768397+yuxuantao12@users.noreply.github.com>
2025-11-18 11:40:20 -0500

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -6,58 +6,58 @@ import unittest
 from calculator import *
 
 class TestCalculator(unittest.TestCase):
-    def test_add():
-        assert add(2, 3) == 5
-        assert add(-1, 1) == 0
-        assert add(0, 0) == 0
+    
+    def test_add(self):
+        self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(-1, 1), 0)
+        self.assertEqual(add(0, 0), 0)
 
 
-    def test_subtract():
-        assert subtract(5, 2) == 3
-        assert subtract(2, 5) == -3
-        assert subtract(0, 0) == 0
+    def test_subtract(self):
+        self.assertEqual(subtract(5, 2), 3)
+        self.assertEqual(subtract(2, 5), -3)
+        self.assertEqual(subtract(0, 0), 0)
     
+
+    def test_divide_by_zero(self):
+        self.assertRaises(ZeroDivisionError, divide, 5, 0)
+
     
-    def test_divide_by_zero():
-        with pytest.raises(ZeroDivisionError):
-            divide(0, 5)
+    def test_logarithm(self):
+        self.assertAlmostEqual(logarithm(10, 100), math.log(100, 10))
+        self.assertEqual(logarithm(2, 8), 3)
     
-    
-    def test_logarithm():
-        assert logarithm(10, 100) == math.log(100, 10)
-        assert logarithm(2, 8) == 3
-    
-    
-    def test_log_invalid_base():
-        with pytest.raises(ValueError):
+
+    def test_log_invalid_base(self):
+        with self.assertRaises(ValueError):
             logarithm(-2, 10)
-        with pytest.raises(ValueError):
+        with self.assertRaises(ValueError):
             logarithm(0, 10)
-        with pytest.raises(ValueError):
+        with self.assertRaises(ValueError):
             logarithm(1, 10)
     
+
+    def test_multiply(self):
+        self.assertEqual(multiply(3, 4), 12)
+        self.assertEqual(multiply(-2, 5), -10)
+        self.assertEqual(multiply(0, 999), 0)
     
-    def test_multiply():
-        assert multiply(3, 4) == 12
-        assert multiply(-2, 5) == -10
-        assert multiply(0, 999) == 0
-    
-    
-    def test_divide():
-        assert divide(2, 10) == 5
-        assert divide(5, -20) == -4
-    
-    
-    def test_log_invalid_argument():
-        with pytest.raises(ValueError):
+
+    def test_divide(self):
+        self.assertEqual(divide(10, 2), 5)
+        self.assertEqual(divide(-20, 5), -4)
+
+
+    def test_log_invalid_argument(self):
+        with self.assertRaises(ValueError):
             logarithm(10, -5)
-        with pytest.raises(ValueError):
+        with self.assertRaises(ValueError):
             logarithm(10, 0)
+
     
-    
-    def test_hypotenuse():
-        assert hypotenuse(3, 4) == 5
-        assert hypotenuse(-3, 4) == 5
+    def test_hypotenuse(self):
+        self.assertEqual(hypotenuse(3, 4), 5)
+        self.assertEqual(hypotenuse(-3, 4), 5)
 
 # Do not touch this
 if __name__ == "__main__":
