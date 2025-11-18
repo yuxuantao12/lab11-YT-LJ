8a6361570e43f19a3e6086e5b88907f194eb277a
Yuxuan Tao <144768397+yuxuantao12@users.noreply.github.com>
2025-11-18 11:36:37 -0500

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -7,9 +7,9 @@ from calculator import *
 
 class TestCalculator(unittest.TestCase):
     def test_add():
-    assert add(2, 3) == 5
-    assert add(-1, 1) == 0
-    assert add(0, 0) == 0
+        assert add(2, 3) == 5
+        assert add(-1, 1) == 0
+        assert add(0, 0) == 0
 
 
     def test_subtract():
