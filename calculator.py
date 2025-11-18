ae5203499dbdb674b3ab39b7ea198f90587a08ae
Yuxuan Tao <144768397+yuxuantao12@users.noreply.github.com>
2025-11-18 11:17:17 -0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,11 +1,51 @@
-"""
-calculator.py
-- Defines functions used to create a simple calculator
+import math
+import pytest
 
-One function per operation, in order.
-"""
-# First example
-def add(a, b): 
-    pass
+
+def square_root(a):
+    try:
+        if a < 0:
+            raise ValueError("Cannot take square root of a negative number.")
+        return math.sqrt(a)
+    except ValueError as e:
+        raise e
+
+
+def hypotenuse(a, b):
+    try:
+        return math.hypot(a, b)
+    except Exception as e:
+        raise e
+
+
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    if a == 0:
+        raise ZeroDivisionError("Cannot divide by zero (a == 0).")
+    return b / a
+
+
+def logarithm(a, b):
+    if a <= 0 or a == 1:
+        raise ValueError("Log base must be positive and not equal to 1.")
+    if b <= 0:
+        raise ValueError("Logarithm argument must be positive.")
+    return math.log(b, a)
+
+
+def exponent(a, b):
+    return a ** b
+
 
 
